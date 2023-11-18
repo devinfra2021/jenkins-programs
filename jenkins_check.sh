a676b8bfd8f6083003d51aa49381f32ab289d5fb
devinfra2021 <freetier12aws@outlook.com>
2023-11-18 04:21:56 +0000

    added new pipeline file

--- jenkins_check.sh
+++ jenkins_check.sh
@@ -4,7 +4,7 @@ declare JENKINS_PORT
 declare JENKINS_URL
 
 export JENKINS_PORT=8080
-export JENKINS_URL=http://x.x.xx.x
+export JENKINS_URL=http://13.232.173.240
 
 function calling_ssh(){
 ssh -p $JENKINS_PORT $JENKINS_URL declarative-linter < Jenkinsfile
